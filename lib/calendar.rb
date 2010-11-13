bf904b217c55c3316f663a525e3e88e5f6faeaa2
yalab <rudeboyjet@gmail.com>
2010-11-13 13:25:29 +0900

    Implement method_missing.

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -1,12 +1,13 @@
 require 'date'
 class Calendar
   attr_reader :first, :last
+
   def self.monthly(year, month)
     self.new(year, month, 1)
   end
 
   def initialize(year, month, day)
-    @first = Date.new(year, month, day)
+    @first = Date.new(year.to_i, month.to_i, day.to_i)
   end
 
   def display
@@ -14,5 +15,10 @@ class Calendar
     @last = @first.next_month - 1
     display_last_day = last + (6 - last.wday)
     (display_first_day..display_last_day)
+  end
+
+  def method_missing(method)
+    super unless @first.respond_to?(method)
+    @first.send(method)
   end
 end
--- test/calendar_test.rb
+++ test/calendar_test.rb
@@ -4,11 +4,17 @@ require 'calendar'
 
 class CalendarTest < Test::Unit::TestCase
   context "monthly" do
-    should "calcuration" do
+    setup do
       @calendar = Calendar.monthly(2010, 11)
+    end
+    should "calcuration" do
       assert_equal Date.new(2010, 10, 31), @calendar.display.first
       assert_equal Date.new(2010, 12, 4), @calendar.display.last
       assert_equal Date.new(2010, 11, 1), @calendar.first
+    end
+    should "get year and month" do
+      assert_equal 2010, @calendar.year
+      assert_equal 11, @calendar.month
     end
   end
 end
